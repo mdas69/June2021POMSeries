cb4de6235d2d6964cafdf4f9cea59519f95833d8
Mangala Das <mdas@example.com>
2021-10-04 07:17:53 +0530

    cart feature addeed

---
+++ src/main/java/com/qa/democart/pages/CartPage.java
@@ -0,0 +1,10 @@
+package com.qa.democart.pages;
+
+import org.openqa.selenium.By;
+
+
+public class CartPage {
+
+By cart= By.id("cart");
+	
+}
---
+++ src/test/java/com/qa/opencart/test/CartTest.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test
+	public void testCart() {
+		Assert.assertEquals(true, true);
+	}
+}
